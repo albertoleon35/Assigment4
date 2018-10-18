2594e29d3ad72db130c6097aa675c114af66ba9c
Alberto Leon <albertoleon325@gmail.com>
2018-10-17 21:43:35 -0700

    Added name, password, labels, states and tab buttons, with validations that enables the buttons

--- assignment4/assignment4/ViewController.swift
+++ assignment4/assignment4/ViewController.swift
@@ -10,11 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let numberOfCharactersDisplay: Int = 3;
+    @IBOutlet weak var nameTextBox: UITextField!
+    @IBOutlet weak var passwordTextBox: UITextField!
+    @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var tabButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func userFinishedEnteringName(_ sender: Any) {
+        guard let nameTextBoxValue = nameTextBox.text else {
+            enableStateAndTabsButton(enabled: false);
+            return;
+        }
+        guard isTextBoxTextGreaterThanThree(value: nameTextBoxValue) else {
+            enableStateAndTabsButton(enabled: false);
+            return;
+        }
+        guard shouldIEnableStateAndTabsButtons() else {
+            enableStateAndTabsButton(enabled: false);
+            return
+        }
+        enableStateAndTabsButton(enabled: true);
+    }
+    
+    @IBAction func userFinishedEnteringPassword(_ sender: Any) {
+        guard let nameTextBoxValue = nameTextBox.text else {
+            enableStateAndTabsButton(enabled: false);
+            return;
+        }
+        guard isTextBoxTextGreaterThanThree(value: nameTextBoxValue) else {
+            enableStateAndTabsButton(enabled: false);
+            return;
+        }
+        
+        guard shouldIEnableStateAndTabsButtons() else {
+            enableStateAndTabsButton(enabled: false);
+            return
+        }
+        enableStateAndTabsButton(enabled: true);
+    }
+    
+    fileprivate func isTextBoxTextGreaterThanThree(value: String) -> Bool {
+        let trimmedString = value.trimmingCharacters(in: .whitespaces)
+        guard !trimmedString.isEmpty && trimmedString.count >= numberOfCharactersDisplay else {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    fileprivate func shouldIEnableStateAndTabsButtons() -> Bool {
+        
+        guard let nameTextBoxValue = nameTextBox.text, let passwordTextBoxValue = passwordTextBox.text, !nameTextBoxValue.isEmpty && !passwordTextBoxValue.isEmpty else {
+            return false;
+        }
+        
+        let name = nameTextBoxValue.trimmingCharacters(in: .whitespaces);
+        let password = passwordTextBoxValue.trimmingCharacters(in: .whitespaces);
+        
+        guard name.count >= numberOfCharactersDisplay  && password.count >= numberOfCharactersDisplay else {
+            return false;
+        }
+        return true;
+    }
+    
+    fileprivate func enableStateAndTabsButton(enabled value: Bool) {
+        stateButton.isEnabled = value;
+        tabButton.isEnabled = value;
+    }
+    
+    
 }
 
